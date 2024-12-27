9101cc860a928144251df3e2ba73f33d5d73cb8b
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-12-27 11:48:19 +0100

    Infolist

--- app/Filament/Resources/TreatmentResource.php
+++ app/Filament/Resources/TreatmentResource.php
@@ -6,11 +6,15 @@
     use App\Filament\Resources\TreatmentResource\RelationManagers;
     use App\Models\Treatment;
     use Filament\Forms\Components\RichEditor;
+    use Filament\Forms\Components;
     use Filament\Forms\Components\Section;
     use Filament\Forms\Form;
+    use Filament\Infolists\Infolist;
     use Filament\Resources\Resource;
     use Filament\Tables;
     use Filament\Tables\Table;
+    use Filament\Infolists\Components\TextEntry;
+    use Filament\Infolists\Components\Section as SectionInfoList;
 
     class TreatmentResource extends Resource
     {
@@ -27,12 +31,12 @@
                 ->schema([
 //                    TextInput::make('patient.name')
 //                        ->label('Paciente'),
-                    Section::make('Descripción de todos los datos')
+                   Section::make('Descripción de todos los datos')
                         ->description('Prevent abuse by limiting the number of requests per period')
                         ->schema([
                             RichEditor::make('description')
                         ]),
-                    Section::make('Notas y otras cosas')
+                   Section::make('Notas y otras cosas')
                         ->description('Prevent abuse by limiting the number of requests per period')
                         ->schema([
                             RichEditor::make('notes')
@@ -40,34 +44,66 @@
                 ]);
         }
 
+        public static function infolist(Infolist $infolist): Infolist
+        {
+
+            return $infolist
+
+           ->schema([
+
+               SectionInfoList::make()
+                   ->schema([
+
+                           TextEntry::make('name'),
+                           TextEntry::make('description')
+                               ->html(),
+                           TextEntry::make('notes')
+                               ->html(),
+                   ])
+                   ->schema([
+                       TextEntry::make('name'),
+                       TextEntry::make('description')
+                           ->html(),
+                       TextEntry::make('notes')
+                           ->html(),
+                   ])
+
+           ]);
+
+        }
         public static function table(Table $table): Table
         {
             return $table
-                 Section::make('Descripción de todos los datos')
-                ->columns([Tables\Columns\TextColumn::make('patient.name')
-                    ->label('Paciente')
-                    ->sortable(),
-                    Tables\Columns\TextColumn::make('description')
-                        ->label('Descripción')
-                        ->html()
-                        ->wrap(),
-                    Tables\Columns\TextColumn::make('notes')
-                        ->label('Notas')
-                        ->html()
-                        ->wrap()
-                ])
-                ->filters([
-                    //
-                ])
-                ->actions([
-                    Tables\Actions\EditAction::make(),
-                ])
-                ->bulkActions([
-                    Tables\Actions\BulkActionGroup::make([
-                        Tables\Actions\DeleteBulkAction::make(),
-                    ]),
-                ]);
+                     ->columns([Tables\Columns\TextColumn::make('patient.name')
+                         ->label('Paciente')
+                         ->sortable(),
+                         Tables\Columns\TextColumn::make('description')
+                             ->label('Descripción')
+                             ->html()
+                             ->wrap(),
+                         Tables\Columns\TextColumn::make('notes')
+                             ->label('Notas')
+                             ->html()
+                             ->wrap()
+                     ])
+                     ->filters([
+                         //
+                     ])
+                     ->actions([
+                         Tables\Actions\ViewAction::make()
+                             ->slideOver()
+                             ->modalWidth('full')
+                             ->button(),
+                         Tables\Actions\EditAction::make()
+                            ->button(),
+                     ])
+                     ->bulkActions([
+                         Tables\Actions\BulkActionGroup::make([
+                             Tables\Actions\DeleteBulkAction::make(),
+                         ]),
+                     ]);
         }
+
 
         public static function getRelations(): array
         {
